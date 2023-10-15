4171ad1f21ab26e77255a848d0a239e604a59976
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-10-15 08:06:05 +0530

    added app Bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'), // Set the fontFamily directly
+      home: const HomePage(),
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Breakfast",
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(color: Color(0xffF7F8F8)),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              width: 37,
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(color: Color(0xffF7F8F8)),
+              child: Text(
+                '..',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ))
+      ],
+    );
   }
 }
